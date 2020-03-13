543c7a13690d1a73c15a295c103b4d0e69faa14d
zheng <849859182@qq.com>
2020-03-13 13:00:32 +0800

    deploy

---
+++ precache-manifest.fc0f6ffa2c4e85da574c5cb6647b9c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f7d6f459b5d16c8099",
+    "url": "/morney-website/css/app.41861f9e.css"
+  },
+  {
+    "revision": "c7b49c8a01575e3fe5b1a4b3999860ae",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "35f7d6f459b5d16c8099",
+    "url": "/morney-website/js/app.8b4ce096.js"
+  },
+  {
+    "revision": "8b7ee468f6af4205fc26",
+    "url": "/morney-website/js/chunk-vendors.1aa09ae5.js"
+  },
+  {
+    "revision": "f0fce8a1d94de3ce6d9839c05c88d85e",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "12a1f4ab053c740f15604462bf2fb99a",
+    "url": "/morney-website/morney-qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
